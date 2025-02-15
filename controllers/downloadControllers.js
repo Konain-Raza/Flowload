d647e164ed1b4d8a14b118135e96f1a1e5835c3a
Konain <konainraza105@gmail.com>
2025-02-15 15:10:44 +0500

    added savefrom net

--- controllers/downloadControllers.js
+++ controllers/downloadControllers.js
@@ -1,5 +1,5 @@
-const puppeteer = require("puppeteer");
 const ytdl = require("@distube/ytdl-core");
+const { savefrom } = require('@bochilteam/scraper-savefrom');
 
 
 const downloadMedia = async (req, res) => {
@@ -7,9 +7,9 @@ const downloadMedia = async (req, res) => {
 
   const videoUrl = req.query.url;
 
-  if (!videoUrl || !ytdl.validateURL(videoUrl)) {
-    return res.status(400).json({ error: "Invalid YouTube URL" });
-  }
+  // if (!videoUrl || !ytdl.validateURL(videoUrl)) {
+  //   return res.status(400).json({ error: "Invalid YouTube URL" });
+  // }
 
   try {
     const options = {
@@ -22,26 +22,29 @@ const downloadMedia = async (req, res) => {
       },
     };    
 
-    const info = await ytdl.getInfo(videoUrl, options);
-    const videoFormat = ytdl.chooseFormat(info.formats, { quality: "highestvideo" });
-    const audioFormat = ytdl.chooseFormat(info.formats, { filter: "audioonly" });
+    // const info = await ytdl.getInfo(videoUrl, options);
+    // const videoFormat = ytdl.chooseFormat(info.formats, { quality: "highestvideo" });
+    // const audioFormat = ytdl.chooseFormat(info.formats, { filter: "audioonly" });
 
-    if (!videoFormat?.url || !audioFormat?.url) {
-      return res.status(500).json({ error: "No valid download links found" });
-    }
+    // if (!videoFormat?.url || !audioFormat?.url) {
+    //   return res.status(500).json({ error: "No valid download links found" });
+    // }
+    const data = await savefrom(videoUrl)
+console.log(data)
 
     res.json({
       success: true,
-      video: {
-        url: videoFormat.url,
-        format: videoFormat.mimeType,
-        quality: videoFormat.qualityLabel,
-      },
-      audio: {
-        url: audioFormat.url,
-        format: audioFormat.mimeType,
-        quality: "Audio Only",
-      },
+      data
+      // video: {
+      //   url: videoFormat.url,
+      //   format: videoFormat.mimeType,
+      //   quality: videoFormat.qualityLabel,
+      // },
+      // audio: {
+      //   url: audioFormat.url,
+      //   format: audioFormat.mimeType,
+      //   quality: "Audio Only",
+      // },
     });
   } catch (error) {
     console.error("Error fetching media:", error);
